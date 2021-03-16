97959826c517aa41c4cc137568d914f73d274602
Gedler <gedler.jeanpierre@stonybrook.edu>
2021-03-16 17:43:27 -0400

    commit tree 57029748a689be67bcb4999fa5ff32b1bd8a56ae

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code here
+
+function saturdayFun(events="roller-skate") { 
+    return `This Saturday, I want to ${events}!`
+}
+function mondayWork (work= "go to the office") {
+    return `This Monday, I will ${work}.` 
+}
+
+
+saturdayFun()
+saturdayFun("bathe my dog!")
+ mondayWork();
+ mondayWork("work from home")
